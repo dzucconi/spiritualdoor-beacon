9b97f26f992a541fd6f045007ede30b6fe966d14
dzucconi <mail@damonzucconi.com>
2016-09-05 12:41:37 +0200

    Fix specs for Queue; add more

--- test/lib/queue.js
+++ test/lib/queue.js
@@ -1,15 +1,19 @@
 import Queue from '../../app/javascripts/lib/queue';
 
+let collection = null;
+
 describe('Queue', () => {
   describe('with a simple collection', () => {
-    const collection = new Queue;
-    collection.enqueue([
-      { id: 'foo' },
-      { id: 'bar' },
-      { id: 'foo' },
-      { id: 'baz' },
-      { id: 'qux' },
-    ]);
+    beforeEach(() => {
+      collection = new Queue;
+      collection.enqueue([
+        { id: 'foo' },
+        { id: 'bar' },
+        { id: 'foo' },
+        { id: 'baz' },
+        { id: 'qux' },
+      ]);
+    });
 
     it('indexes by the ID', () => {
       collection.length().should.equal(4);
@@ -34,8 +38,9 @@ describe('Queue', () => {
 
       it('should return `true` indicating we are at the end', () => {
         collection.dequeue()[1].should.be.false();
-        collection.length().should.equal(1);
-
+        collection.length().should.equal(3);
+        collection.dequeue()[1].should.be.false();
+        collection.dequeue()[1].should.be.false();
         collection.dequeue()[1].should.be.true();
         collection.length().should.equal(0);
         collection.total().should.equal(4);
@@ -43,13 +48,34 @@ describe('Queue', () => {
     });
 
     it('maintains the index so even if we re-add an object we have seen, it is not get queued', () => {
+      collection.length().should.equal(4);
+
       collection.enqueue([
         { id: 'foo' },
         { id: 'new' },
         { id: 'bar' },
       ]);
 
-      collection.length().should.equal(1);
+      collection.length().should.equal(5);
+    });
+
+    it('indexes using the passed function', () => {
+      const another = new Queue(x => `${x.foo}:${x.bar}`);
+      another.enqueue([
+        { id: 'x', foo: 'monday', bar: 'january' },
+        { id: 'y', foo: 'tuesday', bar: 'january' },
+        { id: 'z', foo: 'tuesday', bar: 'february' },
+        { id: 'x', foo: 'sunday', bar: 'december' },
+        { id: 'q', foo: 'monday', bar: 'january' }, // dup
+      ]);
+
+      another.current.map(({ id }) => id)
+        .should.eql([
+          'x',
+          'y',
+          'z',
+          'x',
+        ]);
     });
   });
 });
