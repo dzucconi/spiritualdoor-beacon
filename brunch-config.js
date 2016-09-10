2be18ea742fd5ef07aa8973ec3eab0cf63472e22
dzucconi <mail@damonzucconi.com>
2016-09-10 13:02:54 +0200

    Adds asset revving

--- brunch-config.js
+++ brunch-config.js
@@ -19,6 +19,17 @@ module.exports = {
       processors: [
         require('autoprefixer')(['last 8 versions'])
       ]
+    },
+    digest: {
+      pattern: /DIGEST\(\/?([^\)]*)\)/g,
+      discardNonFilenamePatternParts: true,
+      referenceFiles: /\.html$/,
+      precision: 8,
+      alwaysRun: false,
+      environments: ['production'],
+      prependHost: null,
+      manifest: '',
+      infixes: []
     }
   }
 };
